7f4009c4ab010cef693834a69c9ea78c8f2531b2
EvanJonQiu <EvanJonQiu@users.noreply.github.com>
2021-04-13 17:30:04 +0800

    noblocking reading and writing

--- src/main/java/com/study/SerialDemoJscWriter/App.java
+++ src/main/java/com/study/SerialDemoJscWriter/App.java
@@ -1,34 +1,92 @@
 package com.study.SerialDemoJscWriter;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import com.fazecast.jSerialComm.SerialPort;
+import com.fazecast.jSerialComm.SerialPortDataListener;
+import com.fazecast.jSerialComm.SerialPortEvent;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
+public class App implements Runnable {
+    
+    DataInputStream in;
+    private boolean checkLoop = false;
+    
+    private int count = 0;
+    
+    final SerialWriteWrapper serialWriteWrapper = new SerialWriteWrapper();
+    final static SerialPort serialPort = SerialWriteWrapper.openComPort("COM2", 115200, 8, 1, 0);
+    
+    public void run() {
+        BufferedReader into = new BufferedReader(new InputStreamReader(System.in));
+        String read;
+        System.out.println("Welcome...");
+        
+        serialWriteWrapper.setSerialPortListener(serialPort, new SerialPortDataListener() {
+
+            public int getListeningEvents() {
+                return SerialPort.LISTENING_EVENT_DATA_AVAILABLE;
+            }
+
+            public void serialEvent(SerialPortEvent event) {
+                if (event.getEventType() != SerialPort.LISTENING_EVENT_DATA_AVAILABLE)
+                    return;
+                
+                byte[] bytes = serialWriteWrapper.readData(serialPort);
+                System.out.println("收到的数据："+ new String(bytes));
+                count++;
+                System.out.println("count: " + count);
+            }
+        });
+        
+        while(!checkLoop) {
+            try {
+                System.out.println("running1");
+                read = into.readLine();
+                System.out.println(read);
+                if(read.equals(".bye"))
+                {
+                    checkLoop = true;
+                }
+                Thread.sleep(500);
+            } catch (IOException e) {
+                System.out.println(e);System.out.println("running2");
+            } catch (InterruptedException ie) {
+                System.out.println(ie);System.out.println("running3");
+            }
+        }
+        System.out.println("running4");
+        serialWriteWrapper.closeComPort(serialPort);
+    }
+    
     public static void main( String[] args ) throws InterruptedException
     {
-        SerialWriteWrapper serialWriteWrapper = new SerialWriteWrapper();
-        SerialPort serialPort = serialWriteWrapper.openComPort("COM2", 115200, 8, 1, 0);
+        //SerialWriteWrapper serialWriteWrapper = new SerialWriteWrapper();
+        //SerialPort serialPort = serialWriteWrapper.openComPort("COM2", 115200, 8, 1, 0);
         //String data = "$GNGLL,4917.236380,N,12043.710968,E,074921.000,A,A*4C\r\n";
         //serialPort.writeBytes(data.getBytes(), data.length());
         
-        File file = new File("\\Com4接收日志解析前(20210323154842).txt");
+        App main = new App();
+        Thread t1 = new Thread(main);
+        t1.start();
+        
+        File file = new File("D:\\Workspaces\\esri\\04_project\\16_大兴安岭林管局\\BD_data\\新建文件夹\\Com4接收日志解析前(20210323154842).txt");
         BufferedReader reader = null;
         try {
             reader = new BufferedReader(new FileReader(file));
             String tempString = null;
             int line = 1;
             while ((tempString = reader.readLine()) != null) {
-                System.out.println("line " + line + ": " + tempString);
+                //System.out.println("line " + line + ": " + tempString);
                 String temp = tempString + "\r\n";
                 serialPort.writeBytes(temp.getBytes(), temp.length());
                 line++;
--- src/main/java/com/study/SerialDemoJscWriter/SerialWriteWrapper.java
+++ src/main/java/com/study/SerialDemoJscWriter/SerialWriteWrapper.java
@@ -1,10 +1,22 @@
 package com.study.SerialDemoJscWriter;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+
 import com.fazecast.jSerialComm.SerialPort;
+import com.fazecast.jSerialComm.SerialPortDataListener;
 
 public class SerialWriteWrapper {
     
-    public SerialPort openComPort(String portName, int baudRate, int dataBits, int stopBits, int parity) {
+    private static final int BUFFER_SIZE = 2000;
+    private byte[] buffer = new byte[BUFFER_SIZE];
+    
+    public SerialWriteWrapper() {
+        Arrays.fill(this.buffer, (byte)0);
+    }
+    
+    public static SerialPort openComPort(String portName, int baudRate, int dataBits, int stopBits, int parity) {
         SerialPort comPort = null;
         
         if (portName == null || "".equals(portName)) {
@@ -18,6 +30,7 @@ public class SerialWriteWrapper {
         comPort.openPort();
         comPort.setComPortParameters(baudRate, dataBits, stopBits, parity);
         comPort.setFlowControl(SerialPort.FLOW_CONTROL_DISABLED);
+        comPort.setComPortTimeouts(SerialPort.TIMEOUT_NONBLOCKING, 0, 0);
         return comPort;
     }
     
@@ -25,5 +38,38 @@ public class SerialWriteWrapper {
         if (serialPort != null && serialPort.isOpen()) {
             serialPort.closePort();
         }
+    }
+    
+    public void setSerialPortListener(SerialPort serialPort, SerialPortDataListener listener) {
+        if (serialPort != null && serialPort.isOpen()) {
+            serialPort.addDataListener(listener);
+        }
+    }
+    
+    public final byte[] readData(SerialPort serialPort) {
+        InputStream is = null;
+
+        try {
+            is = serialPort.getInputStream();
+            int buffLength = is.available();
+            
+            while (buffLength > 0) {
+                Arrays.fill(this.buffer, (byte)0);
+                is.read(this.buffer);
+                buffLength = is.available();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (is != null) {
+                    is.close();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        
+        return this.buffer;
     }
 }
